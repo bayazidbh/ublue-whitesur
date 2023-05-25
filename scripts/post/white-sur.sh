a8b6560e5a1479506885908fd26369424c4b66c8
Bayazid Bustami Halim <bayazid.bustami.halim@gmail.com>
2023-05-25 20:49:27 +0700

    install white-sur theme

---
+++ scripts/post/white-sur.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# Tell this script to exit if there are any errors.
+# You should have this in every custom script, to ensure that your completed
+# builds actually ran successfully without any errors!
+set -oue pipefail
+
+# Install white-sur themes
+mkdir -p /tmp/whitesur
+
+git clone https://github.com/vinceliuice/WhiteSur-gtk-theme /tmp/whitesur/WhiteSur-gtk-theme
+/tmp/whitesur/WhiteSur-gtk-theme/install.sh -m -i standard -b default
+
+git clone https://github.com/vinceliuice/WhiteSur-icon-theme /tmp/whitesur/WhiteSur-icon-theme
+/tmp/whitesur/WhiteSur-icon-theme/install.sh
+
+git clone https://github.com/vinceliuice/WhiteSur-kde /tmp/whitesur/WhiteSur-kde
+/tmp/whitesur/WhiteSur-kde/install.sh
+/tmp/whitesur/WhiteSur-kde/sddm/install.sh
+
+git clone https://github.com/vinceliuice/WhiteSur-cursors /tmp/whitesur/WhiteSur-cursors
+/tmp/whitesur/WhiteSur-cursors/install.sh
+
+git clone https://github.com/vinceliuice/Monterey-kde /tmp/whitesur/Monterey-kde
+/tmp/whitesur/Monterey-kde/install.sh
+/tmp/whitesur/Monterey-kde/sddm/install.sh
