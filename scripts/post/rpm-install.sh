bd3b5f47557718f82551ecfe10d481e810ce32ef
Bayazid Bustami Halim <bayazid.bustami.halim@gmail.com>
2023-05-26 23:07:26 +0700

    edit script

--- scripts/post/rpm-install.sh
+++ scripts/post/rpm-install.sh
@@ -7,7 +7,8 @@
 
 # Your code goes here.
 rpm-ostree install https://download.teamviewer.com/download/linux/teamviewer.x86_64.rpm
-rpm-ostree override replace ./post/steam-1.0.0.78-1.fc38.i686.rpm
+
+rpm-ostree override replace /scripts/post/steam-1.0.0.78-1.fc38.i686.rpm
 
 # rpm-ostree install https://download.teamviewer.com/download/linux/teamviewer.x86_64.rpm
 # rpm-ostree install https://github.com/bayazidbh/ublue-kinoite-customized/raw/live/scripts/post/rpm/epson-inkjet-printer-escpr-1.7.26-1lsb3.2.x86_64.rpm
